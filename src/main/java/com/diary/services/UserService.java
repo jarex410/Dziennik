c9b8bf9f6ffb33dca70b4efd685ec28a65054af3
jpadjasek <padjasekjaroslaw@gmail.com>
2016-12-27 22:28:22 +0100

    Brakujaca annotacja

--- src/main/java/com/diary/services/UserService.java
+++ src/main/java/com/diary/services/UserService.java
@@ -4,6 +4,7 @@ import com.diary.dao.UserDAO;
 import com.diary.model.DiaryUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by JaroLP on 2016-12-27.
@@ -14,6 +15,7 @@ public class UserService {
     @Autowired
     UserDAO userDAO;
 
+    @Transactional
     public DiaryUser findByLogin(String login) {
         return userDAO.findByLogin(login);
     }
