2fcf2aa4c1c25fd2419b64272f3a665b1a03dfa0
jpadjasek <padjasekjaroslaw@gmail.com>
2016-12-30 15:34:58 +0100

    Fix dla kontrolera

--- src/main/java/com/diary/controller/SubjectController.java
+++ src/main/java/com/diary/controller/SubjectController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
+import java.util.List;
 
 /**
  * Created by JaroLP on 2016-11-06.
@@ -45,8 +46,8 @@ public class SubjectController {
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public void getSubjectByTeacher(@PathParam("teacherID") String teacherID){
-        subjectService.getSubjectByTeacherId(teacherID);
+    public List<Subject> getSubjectByTeacher(@PathParam("teacherID") String teacherID) {
+        return subjectService.getSubjectByTeacherId(teacherID);
     }
 
 }
